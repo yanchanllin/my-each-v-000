b6c8b2e99c896963c9690a364cef355ab42d4419
YanChan  Lin <yonalin1@gmail.com>
2017-10-26 18:01:27 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,5 +1,5 @@
 def my_each(array)
-  if block_given?
+  # if block_given?
    i = 0
 
    while i < array.length
@@ -8,7 +8,7 @@ def my_each(array)
    end
 
    array
- else
-   puts "Hey! No block was given!"
- end
+ # else
+ #   puts "Hey! No block was given!"
+ # end
 end
